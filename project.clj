84247995aae5d82c9dabfc1e68d4f5b8a6319d6d
Mor Gazith <mor.gazith@benchmarkemail.com>
2022-12-25 16:24:15 +0200

    upgrade to AWS SDK 1.11.16 which supports CredentialProvider that picks up ECS task role credentials.
    Required for running services as ECS tasks

--- project.clj
+++ project.clj
@@ -1,19 +1,26 @@
-(defproject fullcontact/full.aws "0.10.9"
+(defproject fullcontact/full.aws "0.10.11"
   :description "Async Amazon Webservices client."
-  :url "https://github.com/fullcontact/full.aws"
+  :url "https://github.com/contactsplusapp/full.aws"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.amazonaws/aws-java-sdk "1.11.0"]
+  :repositories [["fullcontact" {:url "https://contactsplus.jfrog.io/artifactory/repo"}]
+                 ["releases" {:url "https://contactsplus.jfrog.io/artifactory/libs-release-local"}]
+                 ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local"}]]
+  :deploy-repositories [["releases" {:url "https://contactsplus.jfrog.io/artifactory/libs-release-local" :creds :gpg}]
+                        ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local"}]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.amazonaws/aws-java-sdk "1.11.16"]
                  [com.taoensso/faraday "1.7.1" ; DynamoDB sugar
                   :exclusions [com.amazonaws/aws-java-sdk-dynamodb joda-time]]
-                 [fullcontact/full.http "0.10.8"]
-                 [fullcontact/full.json "0.10.0"]
+                 [fullcontact/full.http "1.0.9"]
+                 [fullcontact/full.json "0.12.0"
+                  :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [fullcontact/full.async "0.9.0"]
                  [fullcontact/full.core "0.10.1"
-                  :exclusions [org.clojure/clojurescript]]]
+                  :exclusions [org.clojure/clojurescript]]
+                 [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.6.0"]]
   :aot :all
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
