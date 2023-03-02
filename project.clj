7651f7e71ef24b54f7af37f650766be42d74e106
Mor Gazith <mor.gazith@benchmarkemail.com>
2023-03-02 15:51:51 +0200

    CON-1564 / Remove ahead-of-time flag from all in-use FullContact libraries

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.aws "0.10.12"
+(defproject fullcontact/full.aws "0.10.13"
   :description "Async Amazon Webservices client."
   :url "https://github.com/contactsplusapp/full.aws"
   :license {:name "Eclipse Public License - v 1.0"
@@ -11,14 +11,13 @@
                         ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local" :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.amazonaws/aws-java-sdk "1.11.16"]
-                 [com.taoensso/faraday "1.7.1" ; DynamoDB sugar
+                 [com.taoensso/faraday "1.11.1" ; DynamoDB sugar
                   :exclusions [com.amazonaws/aws-java-sdk-dynamodb joda-time]]
-                 [fullcontact/full.http "1.0.10"]
-                 [fullcontact/full.json "0.10.2"]
-                 [fullcontact/full.async "0.9.0"]
-                 [fullcontact/full.core "0.10.1"
+                 [fullcontact/full.http "1.0.11"]
+                 [fullcontact/full.json "0.10.3"]
+                 [fullcontact/full.async "1.1.1"]
+                 [fullcontact/full.core "1.1.3"
                   :exclusions [org.clojure/clojurescript]]]
-  :aot :all
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
